f9a3b97524ea6afcc500d41173e75036ed00ba10
Administrador <Administrador@10.10.4.14>
2019-09-19 16:47:44 -0300

    oi

---
+++ empresa/src/controlador/ControladorEmpresa.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import dao.DaoEmpresa;
+import javax.swing.JOptionPane;
+import modelo.Empresa;
+import tela.manutencao.ManutencaoEmpresa;
+import tela.manutencao.ManutencaoEmpresa;
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorEmpresa {
+
+    public static void inserir(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        objeto.setNome_fantasia(man.jtfNomeFantasia.getText());
+        objeto.setRazao_social(man.jtfRazaoSocial.getText());
+        
+        boolean resultado = DaoEmpresa.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+        } else { 
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+
+    public static void alterar(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome_fantasia(man.jtfNomeFantasia.getText());
+        objeto.setRazao_social(man.jtfRazaoSocial.getText());
+        
+        boolean resultado = DaoEmpresa.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+    public static void excluir(ManutencaoEmpresa man){
+        Empresa objeto = new Empresa();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = DaoEmpresa.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+            if (man.listagem != null) {
+     atualizarTabela(man.listagem.tabela); //atualizar a tabela da listagem
+}
+man.dispose();//fechar a tela da manutenção
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+        
+    }
+    public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nome_fantasia");
+        modelo.addColumn("Razao_social");
+        List<Empresa> resultados = DaoEmpresa.consultar();
+        for (Empresa objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome_fantasia());
+            linha.add(objeto.getRazao_social());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+    public static void atualizaCampos(ManutencaoEmpresa man, Integer pk){ 
+        Empresa objeto = DaoEmpresa.consultar(pk);
+        //Definindo os valores do campo na tela (um para cada atributo/campo)
+        man.jtfCodigo.setText(objeto.getCodigo().toString());
+        man.jtfNomeFantasia.setText(objeto.getNome_fantasia());
+        man.jtfRazaoSocial.setText(objeto.getRazao_social());
+        
+        man.jtfCodigo.setEnabled(false); //desabilitando o campo código
+        man.btnAdicionar.setEnabled(false); //desabilitando o botão adicionar
+    }
+}
